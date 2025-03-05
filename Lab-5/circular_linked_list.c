8673ee1a6b06fc2a011f400c637a59ec037e21c9
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-05 09:43:19 +0530

    Completed doubly linked list

---
+++ Lab-5/circular_linked_list.c
@@ -0,0 +1,10 @@
+// 	1. Create and traverse Circular Linked List
+//  2. Insert a node at specific location in a Circular Linked List
+//  3. Delete a node from Circular Linked List
+//
+//  4. Suppose we have 2 circular linked list, form a new circular linked list by joining those 2 lists.
+//  5. Assume that you have 2 sorted circular linked lists, merge them to form another sorted circular linked list.
+//
+#include <stdio.h>
+
+int main() { return 0; }
